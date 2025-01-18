00342d03c791c2d01ac7ce19cc6032b7fb9c01f5
Davor Minchorov <davorminchorov@gmail.com>
2025-01-19 00:18:10 +0100

    Update routes for job positions, categories, and applications

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\JobPositions;
+use App\Livewire\JobPositionDetails;
+use App\Livewire\Categories;
 
 Route::view('/', 'welcome');
 
@@ -11,5 +14,9 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('/', JobPositions::class)->name('positions.index');
+Route::get('/positions/{position}', JobPositionDetails::class)->name('positions.show');
+Route::get('/categories', Categories::class)->name('categories.index');
 
 require __DIR__.'/auth.php';
