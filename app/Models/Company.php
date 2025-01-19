f8f61ff949bdeeca0166d05dbd480727b78a49fb
Davor Minchorov <davorminchorov@gmail.com>
2025-01-19 06:37:02 +0100

    Add enum casting for JobPosition and Company models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\CompanySize;
+use App\Enums\IndustryName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -22,6 +24,12 @@ class Company extends Model
         'founded_year',
         'email',
         'phone',
+    ];
+
+    protected $casts = [
+        'industry' => IndustryName::class,
+        'size' => CompanySize::class,
+        'founded_year' => 'integer',
     ];
 
     public function jobPositions(): HasMany
--- app/Models/JobPosition.php
+++ app/Models/JobPosition.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\JobType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -23,6 +24,11 @@ class JobPosition extends Model
         'salary_range',
         'location',
         'type',
+    ];
+
+    protected $casts = [
+        'type' => JobType::class,
+        'benefits' => 'array',
     ];
 
     public function company(): BelongsTo
