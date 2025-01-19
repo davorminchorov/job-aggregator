b5ad242bf5116c288c57b15756ad36fa892e79c0
Davor Minchorov <davorminchorov@gmail.com>
2025-01-20 00:17:42 +0100

    Rename AuthServiceProvider to PulseServiceProvider

--- app/Providers/PulseServiceProvider.php
+++ app/Providers/PulseServiceProvider.php
@@ -6,17 +6,8 @@ use App\Enums\RoleName;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
-class AuthServiceProvider extends ServiceProvider
+class PulseServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
-
     /**
      * Register any authentication / authorization services.
      */
