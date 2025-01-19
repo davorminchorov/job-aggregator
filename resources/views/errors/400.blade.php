70a84dc65616451473fc30771e906ea8a6c8b5d1
Davor Minchorov <davorminchorov@gmail.com>
2025-01-19 06:23:26 +0100

    Fix home route in error pages

--- resources/views/errors/400.blade.php
+++ resources/views/errors/400.blade.php
@@ -20,7 +20,7 @@
                         <a href="{{ url()->previous() }}" class="inline-flex items-center rounded-md border border-transparent bg-violet-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 dark:focus:ring-offset-slate-900">
                             Go back
                         </a>
-                        <a href="{{ route('home') }}" class="inline-flex items-center rounded-md border border-transparent bg-violet-100 px-4 py-2 text-sm font-medium text-violet-700 hover:bg-violet-200 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 dark:bg-violet-900 dark:text-violet-300 dark:hover:bg-violet-800 dark:focus:ring-offset-slate-900">
+                        <a href="/" class="inline-flex items-center rounded-md border border-transparent bg-violet-100 px-4 py-2 text-sm font-medium text-violet-700 hover:bg-violet-200 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 dark:bg-violet-900 dark:text-violet-300 dark:hover:bg-violet-800 dark:focus:ring-offset-slate-900">
                             Return home
                         </a>
                     </div>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -17,7 +17,7 @@
                         <p class="mt-3 text-base text-slate-600 dark:text-slate-400">Sorry, we're experiencing some technical difficulties. Our team has been notified and is working to fix the issue.</p>
                     </div>
                     <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-                        <a href="{{ route('home') }}" class="inline-flex items-center rounded-md border border-transparent bg-violet-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 dark:focus:ring-offset-slate-900">
+                        <a href="/" class="inline-flex items-center rounded-md border border-transparent bg-violet-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 dark:focus:ring-offset-slate-900">
                             Go back home
                         </a>
                         <a href="{{ route('positions.index') }}" class="inline-flex items-center rounded-md border border-transparent bg-violet-100 px-4 py-2 text-sm font-medium text-violet-700 hover:bg-violet-200 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 dark:bg-violet-900 dark:text-violet-300 dark:hover:bg-violet-800 dark:focus:ring-offset-slate-900">
