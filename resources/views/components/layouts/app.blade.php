add991ddfb12ce998da2575940a5315c1ce67474
Davor Minchorov <davorminchorov@gmail.com>
2025-01-19 02:00:57 +0100

    Replace theme icons with an animated toggle switch

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -66,21 +66,15 @@
                     <!-- Theme toggle -->
                     <button
                         x-on:click="darkMode = !darkMode"
-                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none"
+                        class="relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none bg-gray-200 dark:bg-indigo-600"
                         title="Toggle theme"
                     >
-                        <span x-show="!darkMode" class="w-5 h-5">
-                            <!-- Moon icon -->
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
-                            </svg>
+                        <span
+                            class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform dark:translate-x-6 translate-x-1"
+                            aria-hidden="true"
+                        >
                         </span>
-                        <span x-show="darkMode" class="w-5 h-5">
-                            <!-- Sun icon -->
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-                            </svg>
-                        </span>
+                        <span class="sr-only">Toggle dark mode</span>
                     </button>
                 </div>
             </div>
