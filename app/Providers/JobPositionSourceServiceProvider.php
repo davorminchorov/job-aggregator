98fc0a5e0c3d4d6a30309433b4eeee7649259de9
Davor Minchorov <davorminchorov@gmail.com>
2025-01-19 23:47:55 +0100

    Remove Airtable and Telegram job position sources

--- app/Providers/JobPositionSourceServiceProvider.php
+++ app/Providers/JobPositionSourceServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use App\Services\JobPositionSourceFactory;
-use App\Services\Sources\AirtableJobPositionSource;
-use App\Services\Sources\TelegramJobPositionSource;
 use Illuminate\Support\ServiceProvider;
 
 class JobPositionSourceServiceProvider extends ServiceProvider
@@ -24,11 +22,6 @@ class JobPositionSourceServiceProvider extends ServiceProvider
     {
         $factory = $this->app->make(JobPositionSourceFactory::class);
 
-        $factory->register('airtable', AirtableJobPositionSource::class);
-        $factory->register('telegram', TelegramJobPositionSource::class);
-
         // Register your sources here
-        // Example:
-        // $factory->register('telegram', TelegramJobPositionSource::class);
     }
 }
--- database/seeders/JobPositionSourceTypeSeeder.php
+++ database/seeders/JobPositionSourceTypeSeeder.php
@@ -12,27 +12,6 @@ class JobPositionSourceTypeSeeder extends Seeder
      */
     public function run(): void
     {
-        JobPositionSourceType::create([
-            'name' => 'Airtable',
-            'key' => 'airtable',
-            'description' => 'Import job positions from Airtable bases',
-            'required_fields' => [
-                'pat' => 'string',
-                'base_id' => 'string',
-                'table_id' => 'string',
-            ],
-            'is_active' => true,
-        ]);
-
-        JobPositionSourceType::create([
-            'name' => 'Telegram',
-            'key' => 'telegram',
-            'description' => 'Import job positions from Telegram groups or channels',
-            'required_fields' => [
-                'bot_token' => 'string',
-                'chat_id' => 'string',
-            ],
-            'is_active' => true,
-        ]);
+        // Add your job position source types here
     }
 }
